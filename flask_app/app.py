25e5ea6b80ff73aba85e58733c4fd6797117931d
Sourav <souravraj664@gmail.com>
2025-01-14 20:43:04 +0530

    docker workflow 6

--- flask_app/app.py
+++ flask_app/app.py
@@ -132,5 +132,5 @@ def predict():
     return render_template('index.html', result=result[0])
     
 
-
-app.run(debug=True,host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
